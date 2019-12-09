83be5914c92e40afd4ec87f491ab2b1fa3d4f818
Keir Badger <keir.badger@acuris.com>
2019-12-09 14:16:23 +0000

    Upgrade module to Terraform 0.12

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_ecs_service" "service" {
-  name            = "${var.name}"
-  cluster         = "${var.cluster}"
-  task_definition = "${var.task_definition}"
-  desired_count   = "${var.desired_count}"
-  iam_role        = "${aws_iam_role.role.arn}"
+  name            = var.name
+  cluster         = var.cluster
+  task_definition = var.task_definition
+  desired_count   = var.desired_count
+  iam_role        = aws_iam_role.role.arn
 
   load_balancer {
-    target_group_arn = "${aws_alb_target_group.target_group.arn}"
-    container_name   = "${var.container_name}"
-    container_port   = "${var.container_port}"
+    target_group_arn = aws_alb_target_group.target_group.arn
+    container_name   = var.container_name
+    container_port   = var.container_port
   }
 
   placement_strategy {
@@ -21,40 +21,44 @@ resource "aws_ecs_service" "service" {
     field = "instanceId"
   }
 
-  depends_on = ["null_resource.alb_listener_arn", "null_resource.alb_arn"]
+  depends_on = [
+    null_resource.alb_listener_arn,
+    null_resource.alb_arn,
+  ]
 }
 
 resource "null_resource" "alb_listener_arn" {
-  triggers {
-    alb_listener_arn = "${var.alb_listener_arn}"
+  triggers = {
+    alb_listener_arn = var.alb_listener_arn
   }
 }
 
 resource "null_resource" "alb_arn" {
-  triggers {
-    alb_name = "${var.alb_arn}"
+  triggers = {
+    alb_name = var.alb_arn
   }
 }
 
 resource "aws_alb_target_group" "target_group" {
-  name = "${replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")}"
+  name = replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")
 
   # port will be set dynamically, but for some reason AWS requires a value
   port                 = "31337"
   protocol             = "HTTP"
-  vpc_id               = "${var.vpc_id}"
-  deregistration_delay = "${var.deregistration_delay}"
+  vpc_id               = var.vpc_id
+  deregistration_delay = var.deregistration_delay
 
   health_check {
-    interval            = "${var.health_check_interval}"
-    path                = "${var.health_check_path}"
-    timeout             = "${var.health_check_timeout}"
-    healthy_threshold   = "${var.health_check_healthy_threshold}"
-    unhealthy_threshold = "${var.health_check_unhealthy_threshold}"
-    matcher             = "${var.health_check_matcher}"
+    interval            = var.health_check_interval
+    path                = var.health_check_path
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+    matcher             = var.health_check_matcher
   }
 
   lifecycle {
     create_before_destroy = true
   }
 }
+
